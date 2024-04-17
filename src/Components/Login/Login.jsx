278ccc5f149eab0f41a7e7ef289726717f8fa5be
yaseenhamdy <yaseenhamdy74@gmail.com>
2024-04-17 18:07:50 +0200

    change in login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Login() {
   return (
+    <>
     <div>Login</div>
+    <div>Login</div>
+    <div>Login</div>
+    <div>Login</div>
+
+    </>
   )
 }
