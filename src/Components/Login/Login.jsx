3c89507beee039ac77cff39c13d026275b557a20
AhmedelAnsariy <ahmed.elansariy@gmail.com>
2024-04-29 02:57:39 +0300

    edit login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,73 +1,154 @@
-import React from 'react'
-import LoginImg from '../../Images/Landing Page-image4.jpg'
-import Style from './Login.module.css'
-import { Link,useNavigate } from 'react-router-dom'
-import * as Yup from 'yup'
-import { useFormik } from 'formik'
-import axios from 'axios'
-import { useState } from 'react'
+import React, { useContext, useState } from "react";
+import photo from "../../Images/photo.jpg";
+import style from "./Login.module.css";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import axios from "axios";
+import { Bars } from "react-loader-spinner";
+import { authContext } from "../../Context/AuthContext";
+
+export default function Register() {
+
+let {setAdminToken} =  useContext(authContext);
 
 
+  let user = {
+    Email: "",
+    Password: "",
+  };
 
-export default function Login() {
+  let [ErrorMsg, setErrorMsg] = useState(null);
+  let [SuccessMse, setSuccessMsg] = useState(null);
 
-  let navigate = useNavigate();
-  const [error, setError] = useState(null);
+  let [IsLoading, setIsLoading] = useState(false);
 
-  let passwordRegex = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/
+  let Navigat = useNavigate();
 
-  let initial = {
-    email: '',
-    password: '',
+  function checkValidate(values) {
+    const errors = {};
+
+    setErrorMsg(null);
+
+    if (!values.Email.includes("@") || !values.Email.includes(".")) {
+      errors.Email = "Invalid email. It should contain '@' and '.' ";
+    }
+
+    if (
+      !values.Password.match(
+        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/
+      )
+    ) {
+      errors.Password =
+        "Invalid Password. It must contain at least one uppercase letter, one lowercase letter, one number, and one special character.";
+    }
+
+    return errors;
   }
-  async function loginSubmit(values) {
-     await axios.post('localhost:8080/api/admin/login', values)
-        .catch((err) => {
-            setError("Error Is Exist");
-        })
-        navigate('/')
-    
+
+  async function SendData(values) {
+    setIsLoading(true);
+    try {
+      let { data } = await axios.post(
+        "https://localhost:7127/api/Accounts/Login",
+        values
+      );
+
+      if (data?.status?.value === "Success" && data?.role === "Admin") {
+        // console.log("inside if", data?.status?.value, data?.role);
+        // console.log(data);
+
+        localStorage.setItem("admintkn",data?.token);
+        setAdminToken(data?.token);
+
+
+        setTimeout(function () {
+          Navigat("/admin");
+        }, 1000);
+        
+      }
+    } catch (error) {
+      console.error("API Error:", error);
+    }
+    setIsLoading(false);
 }
-  let validationSchema = Yup.object({
-    email: Yup.string().email('Invalid Email').required('Email Is Requiered'),
-    password: Yup.string().matches(passwordRegex, 'Invalid  Password').required('Password Is Required'),
-  })
-  let loginFormik = useFormik({
-    initialValues: initial,
-    validationSchema,
-    onSubmit: loginSubmit
-    })
+
+
+  let formikObject = useFormik({
+    initialValues: user,
+    validate: checkValidate,
+    onSubmit: SendData,
+  });
 
   return (
     <>
-      <div className="row w-100">
-        <div className="col-md-6 px-0">
-          <div className="loginIMG">
-            <img src={LoginImg} alt="img" className='w-100' />
+      <div className="container my-5 p-4">
+        <div className="row gx-5 d-flex align-items-center">
+          <div className="col-md-6 d-none d-md-block">
+            <img src={photo} className="w-100 vh-100" alt="" />
           </div>
-        </div>
-        <div className={`col-md-6 ${Style['main-background']} px-0`}>
-          <div className={`loginContent d-flex h-75 align-items-center`}>
-            <div className='w-100'>
-              <h3 className={`text-center ${Style['main-color']}`}>Login</h3>
-              <p className={`text-center ${Style['main-color']}`}>We are glad to see you again</p>
-              <div className={`${Style['loginInputs']}`}>
-                <form onSubmit={loginFormik.handleSubmit} className='text-center' >
 
-                  <input onChange={loginFormik.handleChange} onBlur={loginFormik.handleBlur} type="email" placeholder='Email' className={`mx-auto ${Style['change-input-color']} border-0 form-control mb-4`} name='email' />
-                  {loginFormik.errors.email && loginFormik.touched.email ? <div className='alert alert-danger p-2 mt-3'>{loginFormik.errors.email}</div> : ''}
+          <div className="col-md-6">
+            <h1 className="text-center text-main fw-bold fs-1">Login Now </h1>
+            <form onSubmit={formikObject.handleSubmit}>
+              <input
+                type="email"
+                className={`form-control my-4 bg-form ${style.myInput} ${style["bg-form"]}`}
+                placeholder="Email"
+                name="Email"
+                id="Email"
+                value={formikObject.values.Email}
+                onChange={formikObject.handleChange}
+                onBlur={formikObject.handleBlur}
+              />
+              {formikObject.errors.Email && formikObject.touched.Email ? (
+                <div className="alert alert-danger mb-5">
+                  {formikObject.errors.Email}
+                </div>
+              ) : (
+                " "
+              )}
 
-                  <input  onChange={loginFormik.handleChange} onBlur={loginFormik.handleBlur} type="password" placeholder='Password' className={`mx-auto ${Style['change-input-color']} border-0 form-control`} name='password' />
-                  {loginFormik.errors.password && loginFormik.touched.password ? <div className='alert alert-danger p-2 mt-3'>{loginFormik.errors.password}</div> : ''}
+              <input
+                type="password"
+                className={`form-control my-4 bg-form ${style.myInput} ${style["bg-form"]}`}
+                placeholder="Password"
+                name="Password"
+                id="Password"
+                value={formikObject.values.Password}
+                onChange={formikObject.handleChange}
+                onBlur={formikObject.handleBlur}
+              />
+              {formikObject.errors.Password && formikObject.touched.Password ? (
+                <div className="alert alert-danger mb-5">
+                  {formikObject.errors.Password}
+                </div>
+              ) : (
+                " "
+              )}
 
-                  <button disabled={!(loginFormik.dirty && loginFormik.isValid)} type='submit' className={`${Style['btn-login']} btn  text-white mt-4`}>Login</button>
-                </form>
-                <p className='text-center mt-4'>Don't have an account yet?<Link to='/register' className={`text-decoration-none ${Style['main-color']}`}> Signup now</Link></p>
-              </div>
-            </div>
+<button
+  className="btn btn-success w-100 p-2"
+  type="submit"
+  disabled={!(formikObject.dirty && formikObject.isValid)}
+>
+  {IsLoading ? (
+    <Bars
+      height="90"
+      width="90"
+      color="#fff"
+      ariaLabel="bars-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  ) : (
+    "Login Now"
+  )}
+</button>
+            </form>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
